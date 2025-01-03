ba2b82282517a6db34ab49a1776da53370ffb0cb
Nejra Adilović <nadilovic2@etf.unsa.ba>
2025-01-03 17:59:39 +0100

    Added verify function to CommonUtility class for checking user login status

--- test/utilities/common-utility.js
+++ test/utilities/common-utility.js
@@ -1,4 +1,5 @@
 const { expect } = require("@wdio/globals");
+const { HomePage } = require("../pageobjects/pageobjects");
 
 class CommonUtility {
   async checkUrlContains(expectedSubstring) {
@@ -9,6 +10,11 @@ class CommonUtility {
   async verifyMessage(messageSelector, expectedMessage) {
     const actualMessage = await messageSelector.getText();
     expect(actualMessage).toContain(expectedMessage);
+  }
+
+  async verifyUserLoggedIn(email) {
+    const expectedUsername = email.split('@')[0];  
+    await this.verifyMessage(HomePage.usernameDropdown, expectedUsername); 
   }
 }
 
